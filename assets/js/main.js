ed3fd4a0ed71800ebe010915b7ce41d22058868e
ashikcse045 <ashikcse321@gmail.com>
2022-11-14 01:52:13 +0600

    update sticky navbar

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,16 @@
 // window.addEventListener("hashchange", () => window.history.pushState({}, "", '/'), {});
 
+
+// SCRIPT FOR HIDE # IN URL
 function hashHandler() {
     const loc = window.location.hash.split('#')[1];
     window.history.pushState({}, 'Page Title', '/' + loc);
 }
 
 window.addEventListener('hashchange', hashHandler, false);
+
+// SCRIPT FOR FLOATING NAVBAR
+window.addEventListener('scroll', function(){
+    let nav = document.querySelector('.header_navbar');
+    nav.classList.toggle("sticky", window.scrollY > 490);
+});
