bceecdf94c0db0eb12898e24995c56ee693e7de1
ashikcse045 <ashikcse321@gmail.com>
2022-11-14 17:40:54 +0600

    update skill progress and added favicon

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,3 +14,39 @@ window.addEventListener('scroll', function(){
     let nav = document.querySelector('.header_navbar');
     nav.classList.toggle("sticky", window.scrollY > 490);
 });
+
+//PROGRESS BAR ANIMATION
+const progress_section = document.querySelector('.skill_box');
+
+const progress_indicator = document.querySelectorAll('.indicator');
+
+function showProgress(){
+    progress_indicator.forEach(progressBar => {
+        const value = progressBar.dataset.progress;
+
+        progressBar.style.opacity = 1;
+        progressBar.style.width = `${value}%`;
+    })
+}
+
+function hideProgress()
+{
+    progress_indicator.forEach(p => {
+        p.style.opacity = 0;
+        p.style.width = 0;
+    })
+}
+
+window.addEventListener('scroll', () => {
+    const selec_pos = progress_section.getBoundingClientRect().top;
+
+    const screen_pos = window.innerHeight;
+
+    if(selec_pos < screen_pos)
+    {
+        showProgress();
+    }
+    else{
+        hideProgress();
+    }
+});
